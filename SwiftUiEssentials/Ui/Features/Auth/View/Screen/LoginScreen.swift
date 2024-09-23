2de51b4f8fbd80679b2a8f09f6793f5a69295fe2
itheamc <itheamc@gmail.com>
2024-09-23 22:54:27 +0545

    completed ui

--- SwiftUiEssentials/Ui/Features/Auth/View/Screen/LoginScreen.swift
+++ SwiftUiEssentials/Ui/Features/Auth/View/Screen/LoginScreen.swift
@@ -8,8 +8,73 @@
 import SwiftUI
 
 struct LoginScreen: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var isVisible: Bool = false
+    
+    @ObservedObject var authViewModel: AuthViewModel = AuthViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Text("Sign In")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                TextField("Email", text: $email)
+                    .keyboardType(.emailAddress)
+                    .textContentType(.emailAddress)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
+                HStack {
+                    if isVisible {
+                        TextField("Password", text: $password)
+                            .textContentType(.password)
+                    } else {
+                        SecureField("Password", text: $password)
+                            .textContentType(.password)
+                            .padding(.vertical, 0.5)
+                    }
+                                
+                    Button(action: { isVisible.toggle() }) {
+                        Image(systemName: isVisible ? "eye.slash.fill" : "eye.fill")
+                            .foregroundColor(.gray)
+                    }
+                }
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .padding(EdgeInsets(top: 8, leading: 16, bottom: 0, trailing: 16))
+                
+                VStack {
+                    Button(action: {
+                        authViewModel.login(payload: LoginRequestPayload(email: email, password: password))
+                    }) {
+                        Text("Login")
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.green.gradient)
+                            .cornerRadius(10)
+                    }
+                    .overlay {
+                        if case .loggingIn = authViewModel.loginState {
+                            ShimmerView()
+                                .mask(RoundedRectangle(cornerRadius: 10))
+                        }
+                    }
+                }
+                    .padding(EdgeInsets(top: 8, leading: 16, bottom: 0, trailing: 16))
+            }
+            
+            
+        }
+            .ignoresSafeArea()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.blue.gradient)
     }
 }
 
--- SwiftUiEssentials/Ui/Shared/ShimmerView.swift
+++ SwiftUiEssentials/Ui/Shared/ShimmerView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct ShimmerView: View {
+    @State private var isAnimating = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            // The gradient used for the shimmer effect
+            let gradient = LinearGradient(
+                gradient: Gradient(colors: [Color.gray.opacity(0.3), Color.gray.opacity(0.1), Color.gray.opacity(0.3)]),
+                startPoint: .leading,
+                endPoint: .trailing
+            )
+            
+            // Shimmering Rectangle
+            Rectangle()
+                .fill(gradient)
+                .rotationEffect(Angle(degrees: 30)) // Adding angle to the gradient
+                .offset(x: isAnimating ? geometry.size.width : -geometry.size.width) // Initial offset
+                .onAppear {
+                    withAnimation(Animation.linear(duration: 1.5).repeatForever(autoreverses: false)) {
+                        isAnimating = true // Triggers the animation
+                    }
+                }
+        }
     }
 }
-
 #Preview {
     ShimmerView()
 }
+
+
