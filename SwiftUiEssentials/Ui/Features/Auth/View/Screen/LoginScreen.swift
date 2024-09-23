fe1d5e52d17560cb74239a88ce5b88ba5741cd87
itheamc <itheamc@gmail.com>
2024-09-23 22:53:30 +0545

    Added login screen and shimmer view

---
+++ SwiftUiEssentials/Ui/Features/Auth/View/Screen/LoginScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LoginScreen.swift
+//  SwiftUiEssentials
+//
+//  Created by Amit on 23/09/2024.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginScreen()
+}
---
+++ SwiftUiEssentials/Ui/Shared/ShimmerView.swift
@@ -0,0 +1,18 @@
+//
+//  ShimmerView.swift
+//  SwiftUiEssentials
+//
+//  Created by Amit on 23/09/2024.
+//
+
+import SwiftUI
+
+struct ShimmerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShimmerView()
+}
